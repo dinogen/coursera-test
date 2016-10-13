3822e67d9f7bbbf39d5575c5d1b8846ffa06ef1e
Semboli Marcello <marcello.semboli@alfazero.local>
2016-10-13 16:52:32 +0200

    extra tasks

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,24 +7,42 @@ angular.module('LunchCheck', [])
 function LunchCheckController ($scope) {
   $scope.foodList = "";
   $scope.message = "";
+  $scope.messageColor = "text-success";
+  $scope.inputBorderColor = "grey";
 
   $scope.checkFood = function () {
     var foodList = $scope.foodList;
 
     if (foodList.length == 0) {
       $scope.message = "Please enter data first";
+      $scope.messageColor = "text-danger";
+      $scope.inputBorderColor = "red";
     } else {
-      var list = foodList.split(",");
-      console.log("the list has " + list.length + " elements");
-      if (list.length > 3) {
+      var numOfItem = countTheList(foodList);
+      console.log("the list has " + numOfItem + " elements");
+      if (numOfItem > 3) {
         $scope.message = "Too much!";
+        $scope.messageColor = "text-success";
+        $scope.inputBorderColor = "green";
       } else {
         $scope.message = "Enjoy!";
+        $scope.messageColor = "text-success";
+        $scope.inputBorderColor = "green";
       }
 
     }
+  }
 
+  function countTheList(list) {
+    var count = 0;
+    var ar = list.split(",");
+    for (var i=0; i<ar.length; i++)  {
+      if (ar[i].trim().length > 0) {
+        count++;
+      }
+    }
 
+    return count;
 
   }
 
